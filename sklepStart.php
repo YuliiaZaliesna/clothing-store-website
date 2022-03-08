0085d20b313263d44afdaca09f786a56acca5b88
YuliiaZaliesna <yuliazalesna2002@gmail.com>
2022-03-08 15:03:25 +0200

    Create sklepStart.php

---
+++ sklepStart.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang = "pl">
+<head>
+    <?php include 'headPartHtml.php'; ?>
+</head>
+<body>
+<header>
+    <?php include 'header.php'; ?>
+</header>
+<article>
+    <?php include 'article.php'; ?>
+</article>
+<footer>
+    <?php include 'footer.php'; ?>
+</footer>
+</body>
+</html>
